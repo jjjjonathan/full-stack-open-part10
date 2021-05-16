b1162cedbc2f85556d7489cde3dd1c8a5008ba44
jjjjonathan <hornathanjon@gmail.com>
2021-05-16 18:33:12 -0500

    exercise 10.11 complete

---
+++ rate-repository-app/src/graphql/queries.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      totalCount
+      edges {
+        node {
+          id
+          name
+          ownerName
+          createdAt
+          fullName
+          reviewCount
+          ratingAverage
+          forksCount
+          stargazersCount
+          description
+          language
+          ownerAvatarUrl
+        }
+        cursor
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+`;
