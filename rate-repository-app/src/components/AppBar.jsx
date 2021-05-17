65a75d1c1efc60c86ff4036fd8d06653c58c832f
jjjjonathan <hornathanjon@gmail.com>
2021-05-16 23:16:04 -0500

    exercise 10.16 complete

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
+import { useQuery } from '@apollo/client';
+import useAuthStorage from '../hooks/useAuthStorage';
+import { useApolloClient } from '@apollo/client';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 
 import theme from '../theme';
+import { GET_AUTHORIZED_USER } from '../graphql/queries';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,11 +19,30 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const authStorage = useAuthStorage();
+  const client = useApolloClient();
+  const { data } = useQuery(GET_AUTHORIZED_USER);
+  console.log('Authorized?', data);
+
+  const handleSignOut = async () => {
+    console.log('signing out!');
+    await authStorage.removeAccessToken();
+    client.resetStore();
+  };
+
+  const signTab = () => {
+    if (!data || !data.authorizedUser) {
+      return <AppBarTab text="Sign In" link="/signin" />;
+    } else {
+      return <AppBarTab text="Sign Out" onPress={handleSignOut} />;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <AppBarTab text="Repositories" link="/" />
-        <AppBarTab text="Sign In" link="/signin" />
+        {signTab()}
       </ScrollView>
     </View>
   );
--- rate-repository-app/src/components/AppBarTab.jsx
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Pressable } from 'react-native';
 import { Link } from 'react-router-native';
 import Text from './Text';
 
-const AppBarTab = ({ text, link }) => {
+const AppBarTab = ({ text, link, onPress }) => {
+  if (onPress) {
+    return (
+      <Pressable onPress={onPress}>
+        <Text fontSize="heading" fontWeight="bold" style={{ padding: 20 }}>
+          {text}
+        </Text>
+      </Pressable>
+    );
+  }
   return (
     <Link to={link}>
       <Text fontSize="heading" fontWeight="bold" style={{ padding: 20 }}>
